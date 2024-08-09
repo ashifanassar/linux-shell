b8fa0c85c818aa6aa77897a12e7ce52a4be6781f
ashifanassar <ashifanassar@gmail.com>
2024-08-09 11:49:47 +0530

    shell scripting

--- debug.sh
+++ debug.sh
@@ -3,11 +3,11 @@
 #debug script with bash -x scriptname.sh
 
 
-
+set -e
 echo "Enter a number:"
 read num
 
-if (( $num % 2 == 0 )); then
+if (( $num %  == 0 )); then
   echo "The number $num is even."
 else
   echo "The number $num is odd."
